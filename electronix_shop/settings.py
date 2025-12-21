0bd3bdc90c562a164c5bebe58c32e9c8d5aea83a
nurshapagat1 <nurshapagat879@gmail.com>
2025-12-21 19:00:04 +0500

    Update in requiremnts

--- electronix_shop/settings.py
+++ electronix_shop/settings.py
@@ -160,3 +160,9 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': ['profile', 'email'],
+        'AUTH_PARAMS': {'access_type': 'online'},
+    }
+}
